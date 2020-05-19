35e4104c4a3aa0e5d8a2f420c9f6bd2223441f4e
Valoctocat <valentin.gabeff@epfl.ch>
2020-05-20 00:07:14 +0200

    Improved first room

--- Assets/Scripts/Controllers/AnimationPlayer.cs
+++ Assets/Scripts/Controllers/AnimationPlayer.cs
@@ -11,57 +11,99 @@ public class AnimationPlayer : MonoBehaviour
     public GameObject _floorAnimator;
     public float max_radius = 20.0f;
     public float min_radius = 1.0f;
+    private float min_scale; //Set to dset scale OnEnter
     public float max_scale = 600.0f;
+    public float scaling_speed;
+    public float raising_speed;
     public OVRScreenFade _fader;
 
+    private bool _forceRaiseDown = false;
+    private bool _forceScaleDown = false;
+    private bool _enlargingCube = false;
+    private bool _raisingCube = false;
+    private bool _animationFinished = false;
 
-    private bool _forceStop = false;
-    private bool _mainAnimation = false;
     private GameObject _dataset;
+    private GameObject _duplicatedDset;
+    private GameObject _waitingDSet;
     private float current_radius;
+    private float current_scale;
 
     private float timer;
+    private float timerScale;
     private Vector3 _floorScaler;
 
     void Start() {
         _floorScaler = new Vector3(_floorAnimator.transform.localScale.x, 0.0f, _floorAnimator.transform.localScale.z);
         current_radius = min_radius;
+        _waitingDSet = null;
+        _dataset = null;
+        _duplicatedDset = null;
     }
 
     void Update()
     {
-        if(!_mainAnimation)  {
-            // Update radius of floor
-            UpdateRadius();
+        if(!_animationFinished) {
+            if(_raisingCube || _forceRaiseDown)  {
+                // Update radius of floor and raising cube altitude
+                UpdateRadius();
 
-            //Update floor
-            UpdateFloor();
-            UpdateDSet();
+                //Update floor
+                UpdateFloor();
+                UpdateDSet();
+            } else if(_enlargingCube || _forceScaleDown) {
+                UpdateScale();
+
+                UpdateRoom();
+                UpdateDSetScale();
+            }
+        }
+
+        if(_waitingDSet != null) {
+            if((!_raisingCube) && (!_enlargingCube) && (!_forceRaiseDown) &&(!_forceScaleDown)) {
+                OnEnter(_waitingDSet);
+            }
         }
     }
 
     private void UpdateRadius() {
-        if(_particleSystem.GetInside() && !_forceStop) {
-            Debug.Log(this.current_radius);
-            this.current_radius += this.timer*0.001f;
-            Debug.Log(this.current_radius);
+        if(_raisingCube && !_forceRaiseDown) {
+            this.current_radius += this.timer*raising_speed;
             this.timer+=1.0f;
 
-            //Reset variables when Radius back to normal
-            if(Mathf.Abs(this.current_radius-min_radius) < 0.0001){
-                //OnFinish();
-            }
-
             if(this.current_radius>max_radius){
-                print("Triggering Main Event");
                 OnTriggerMainEvent();
             }
 
         } else {
             this.current_radius -= 3.0f*Time.deltaTime;
+
+            //Reset variables when Radius back to normal
+            if(this.current_radius < min_radius){
+                OnFinishRetraction();
+            }
         }
 
-        this.current_radius = Mathf.Clamp(this.current_radius, 1.0f, max_radius);
+        this.current_radius = Mathf.Clamp(this.current_radius, min_radius, max_radius);
+    }
+
+    private void UpdateScale() {
+        if(_enlargingCube && !_forceScaleDown) {
+            this.current_scale += this.timerScale*scaling_speed;
+            this.timerScale+=1.0f;
+
+            if(this.current_scale>max_scale) {
+                OnFinishMainEvent();
+            }
+        } else {
+            this.current_scale -= 2.0f;
+            print("UpdateScaleDown");
+            if(this.current_scale < min_scale) {
+                OnFinishScalingDown();
+            }
+        }
+
+        this.current_scale = Mathf.Clamp(this.current_scale, min_scale, max_scale);
     }
 
 
@@ -72,51 +114,108 @@ public class AnimationPlayer : MonoBehaviour
     }
 
     private void UpdateDSet() {
-      if(_dataset != null) {
-            _dataset.transform.position = new Vector3(_dataset.transform.position.x, 0.45f + current_radius/10.0f, _dataset.transform.position.z);
-            _dataset.transform.Rotate(0,5.0f*Time.deltaTime,0);
-      }
+        if(_duplicatedDset != null) {
+              _duplicatedDset.transform.position = new Vector3(_duplicatedDset.transform.position.x, 0.45f + current_radius/20.0f, _duplicatedDset.transform.position.z);
+              _duplicatedDset.transform.Rotate(0,5.0f*Time.deltaTime,0);
+        }
     }
 
+    private void UpdateRoom() {
+        if(_roomScaler != null) {
+              _roomScaler.UpdateScale(current_scale);
+        }
+    }
+
+    private void UpdateDSetScale() {
+        if(_duplicatedDset != null) {
+            _duplicatedDset.transform.localScale = current_scale * Vector3.one;
+        }
+    }
 
     public void OnEnter(GameObject dataset) {
-        timer = 1.0f;
-        _dataset = dataset;
+        if((_dataset == null) && (!_raisingCube) && (!_enlargingCube) && (!_forceRaiseDown) &&(!_forceScaleDown)) {
+            timer = 1.0f;
+            _waitingDSet = null;
+            _dataset = dataset;
+            min_scale = _dataset.transform.localScale.x;
+
+            //Create Phantom Cube
+            _duplicatedDset = Instantiate(_dataset);
+            _duplicatedDset.GetComponent<Rigidbody>().isKinematic = true;
+            Destroy(_duplicatedDset.GetComponent<OVRGrabbable>());
+            Destroy(_duplicatedDset.GetComponent<Rigidbody>());
+            Destroy(_duplicatedDset.GetComponent<Collider>());
+            Destroy(_duplicatedDset.GetComponent<CubeController>());
+            _raisingCube = true;
+        } else {
+            _waitingDSet = dataset;
+        }
     }
 
     public void OnExit() {
-        _forceStop = true;
+
+
+        if(_animationFinished || _enlargingCube) {
+          // Make cube small again, then make it go Down
+          // MAke room right scale
+          _forceScaleDown = true;
+          _enlargingCube = false;
+          _animationFinished = false;
+
+        } else if(_raisingCube) {
+          // Make cube go down again.
+          _raisingCube = false;
+          _forceRaiseDown = true;
+        }
     }
 
-    public void OnFinish() {
-        _forceStop = false;
+    // Called when the dataset position is reset back to normal.
+    public void OnFinishRetraction() {
+        _forceRaiseDown = false;
         timer = 1.0f;
         _dataset = null;
+        _raisingCube = false;
+        Destroy(_duplicatedDset);
+    }
+
+    public void OnFinishScalingDown() {
+        _forceScaleDown = false;
+        _forceRaiseDown = true;
+        _enlargingCube = false;
+        timerScale = 1.0f;
     }
 
     private void OnTriggerMainEvent() {
-        _mainAnimation = true;
-        _forceStop = false;
-        DATASET_NAME = _dataset.name;
+        if(_dataset != null) {
+            _enlargingCube = true;
+            _raisingCube = false;
+            timerScale = 1.0f;
+            timer = 1.0f;
+        }
+    }
 
-        StartCoroutine("EnlargeCube");
-        _roomScaler.Triggered();
+    private void OnFinishMainEvent() {
+        _animationFinished = true;
+        _enlargingCube = false;
+        _raisingCube = false;
+        DATASET_NAME = _dataset.name;
+        timerScale = 1.0f;
     }
 
     private IEnumerator EnlargeCube() {
-        _dataset.GetComponent<Rigidbody>().isKinematic = true;
-        float scale = _dataset.transform.localScale.x;
+        float scale = _duplicatedDset.transform.localScale.x;
         int i=0;
-        while(_dataset.transform.localScale.x < max_scale) {
-            scale += 0.05f*i;
-            _dataset.transform.localScale += Vector3.one*scale;
+        while(_duplicatedDset.transform.localScale.x < max_scale) {
+            scale += scaling_speed*i;
+            _duplicatedDset.transform.localScale += Vector3.one*scale;
             i+=1;
-            yield return null;
+            yield return new WaitForSeconds(0.02f);
         }
 
         //Fade to black
         _fader.FadeIn();
+        _animationFinished = true;
+        _enlargingCube = false;
         yield break;
-
     }
 }
--- Assets/Scripts/Controllers/RoomScaler.cs
+++ Assets/Scripts/Controllers/RoomScaler.cs
@@ -19,20 +19,29 @@ public class RoomScaler : MonoBehaviour
 
     public void Triggered() {
         if(room != null && !alreadyTriggered) {
+            print("Triggered");
             StartCoroutine("ScaleRoom");
             alreadyTriggered = true;
         }
         return;
     }
 
+    public void UpdateScale(float scale) {
+        float final_scale = Mathf.Clamp(scale, current_scale, end_scale);
+        this.room.transform.localScale = final_scale * Vector3.one;
+    }
+
     IEnumerator ScaleRoom() {
+      print("ScaleRoom");
       yield return new WaitForEndOfFrame();
       int i =1;
+      print(current_scale + " " + end_scale);
       while(current_scale < end_scale) {
           current_scale += scaling_speed*i;
+          print(current_scale);
           this.room.transform.localScale = current_scale * Vector3.one;
           i++;
-          yield return null;
+          yield return new WaitForSeconds(0.02f);
       }
       yield break;
     }
